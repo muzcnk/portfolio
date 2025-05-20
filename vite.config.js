7fadec62b26f1de6c3077b0b04173a9ae9d7fac4
mertuzun <jmert972@gmail.com>
2025-05-20 23:32:30 +0300

    vite.config.js dosya değişiklikleri

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // ✅ Bu satır önemli
   plugins: [react()],
   server: {
     port: 5173,
-    open: true, // Tarayıcıyı otomatik aç
-    host: true // Network üzerinden erişime izin ver
+    open: true,
+    host: true
   }
 })
